49b755a23de4e297e9038807e95f26b999740e1e
David Anderson <david@ip-192-168-30-17.ec2.internal>
2019-12-29 21:43:59 -0500

    first try github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 
 # Set up SSH Keys
 mkdir ~/.ssh
-echo "$SSH_PRIVATE_KEY" > ~/.ssh/honeypot_ec2-user.pem
+echo "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/honeypot_ec2-user.pem
 chmod 400 ~/.ssh/honeypot_ec2-user.pem
-echo "$SSH_PUBLIC_KEY" > ~/.ssh/honeypot_ec2-user.pub
+echo "$INPUT_SSH_PUBLIC_KEY" > ~/.ssh/honeypot_ec2-user.pub
 chmod 400 ~/.ssh/honeypot_ec2-user.pub
 
 # Set up AWS CLI
 mkdir ~/.aws
 echo "[default]" > ~/.aws/credentials
-echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
-echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+echo "aws_access_key_id = $INPUT_AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
+echo "aws_secret_access_key = $INPUT_AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
 
 # Run ansible playbook
 cd /app/ansible
